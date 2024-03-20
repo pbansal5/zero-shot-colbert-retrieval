5202a57d62709a7c59634f6adc7fb70218bd6bc9
Hprairie <hprairie17@gmail.com>
2024-03-19 19:54:54 -0500

    began debugging

--- src/cbralm/bm25_retrieval.py
+++ src/cbralm/bm25_retrieval.py
@@ -1,5 +1,5 @@
 import datasets
-from pathlib import Path,PurePath
+from pathlib import Path, PurePath
 from pyserini.search.lucene import LuceneSearcher
 import argparse
 from transformers import AutoTokenizer
@@ -7,22 +7,23 @@ import json
 import tqdm
 import os
 
-def not_forbidden(context,forbidden_titles):
+
+def not_forbidden(context, forbidden_titles):
     title, _ = context.split("\n")
     if title.startswith('"') and title.endswith('"'):
         title = title[1:-1]
-    return (title not in forbidden_titles)
+    return title not in forbidden_titles
+
 
 def get_bm25_documents(args):
-    output_json = {} 
-    query_to_retrieved_docs = [] 
-
+    output_json = {}
+    query_to_retrieved_docs = []
 
     if args.forbidden_titles:
         with open(args.forbidden_titles, "r") as f:
             forbidden_titles = [line.strip() for line in f]
         forbidden_titles = set(forbidden_titles)
-    else: 
+    else:
         forbidden_titles = set([])
 
     if args.data_dir:
@@ -34,65 +35,92 @@ def get_bm25_documents(args):
     #         query_corpus = datasets.load_dataset('wikitext','wikitext-103-v1')['test']['text']
     #     case default:
     #         raise Exception("Unknown Query Corpus")
-    
-    if args.query_corpus == 'wikitext':
-        query_corpus = datasets.load_dataset('wikitext','wikitext-103-v1')['test']['text']
-    else : 
+
+    if args.query_corpus == "wikitext":
+        query_corpus = datasets.load_dataset("wikitext", "wikitext-103-v1")["test"][
+            "text"
+        ]
+    else:
         raise Exception("Unknown Query Corpus")
-    
-    query_corpus = ' '.join(query_corpus).strip()
+
+    query_corpus = " ".join(query_corpus).strip()
     searcher = LuceneSearcher.from_prebuilt_index(args.retrieval_corpus)
 
-    ## We tokenize the query corpus and untokenize it since the stride is in terms of number of tokens instead of number of words
+    ## We tokenize the query corpus and untokenize it since the stride
+    ## is in terms of number of tokens instead of number of words
 
-    num_docs_to_retrieve = max(4*args.topK,100)
+    num_docs_to_retrieve = max(4 * args.topK, 100)
 
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
-    tokenized_query_corpus = tokenizer(query_corpus,return_tensors='np')['input_ids'][0]
+    tokenized_query_corpus = tokenizer(query_corpus, return_tensors="np")["input_ids"][
+        0
+    ]
     length_query_corpus = len(tokenized_query_corpus)
 
-    for start in tqdm.tqdm(range(0,length_query_corpus,args.retrieval_stride)):
+    for start in tqdm.tqdm(
+        range(0, length_query_corpus, args.retrieval_stride)[:500]
+    ):  # Change this after debugging
         end = start + args.retrieval_query_length
         tokenized_query_segment = tokenized_query_corpus[start:end]
         query_segment = tokenizer.decode(tokenized_query_segment)
-        hits = searcher.search(query_segment,num_docs_to_retrieve)
-        
-        # some paragraphs are shared in the retrieval corpus and wikitext-103. Hence these paragraphs need to be removed from the retrieval corpus
-        # This follows in-context RALM paper. 
-        filtered_hits = [hit for hit in hits if not_forbidden(json.loads(hit.raw)['contents'],forbidden_titles)]
+        hits = searcher.search(query_segment, num_docs_to_retrieve)
 
-        filtered_hits_topk = [dict({'rank':i,'docid':filtered_hits[i].docid,'score':filtered_hits[i].score, 'text': filtered_hits[i].contents}) for i in range(args.topK)]
-        query_to_retrieved_docs.append(
+        # some paragraphs are shared in the retrieval corpus and wikitext-103.
+        # Hence these paragraphs need to be removed from the retrieval corpus
+        # This follows in-context RALM paper.
+        filtered_hits = [
+            hit
+            for hit in hits
+            if not_forbidden(json.loads(hit.raw)["contents"], forbidden_titles)
+        ]
+
+        filtered_hits_topk = [
+            dict(
                 {
-                    'begin_location': start,
-                    'end_location': end,
-                    'query_seg': query_segment,
-                    'retrieved_docs': filtered_hits_topk
+                    "rank": i,
+                    "docid": filtered_hits[i].docid,
+                    "score": filtered_hits[i].score,
+                    "text": filtered_hits[i].contents,
                 }
-        ) 
+            )
+            for i in range(args.topK)
+        ]
+        query_to_retrieved_docs.append(
+            {
+                "begin_location": start,
+                "end_location": end,
+                "query_seg": query_segment,
+                "retrieved_docs": filtered_hits_topk,
+            }
+        )
 
-    output_json['query_to_retrieved_docs'] = query_to_retrieved_docs
-    output_json['bm25_logging_info'] = vars(args)
+    output_json["query_to_retrieved_docs"] = query_to_retrieved_docs
+    output_json["bm25_logging_info"] = vars(args)
     return output_json
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir', type=str, default=None) # '/datastor1/pbansal/huggingface_cache', /home/pb25659/huggingface_cache
-    parser.add_argument('--output_file', type=str, default=None) # 
-    parser.add_argument('--retrieval_corpus', type=str, default=None) # wikipedia-dpr-100w
-    parser.add_argument('--query_corpus', type=str, default=None) # wikipedia-dpr-100w
-    parser.add_argument('--topK', type=int, default=16) # 16
-    parser.add_argument('--retrieval_query_length', type=int, default=32) # 32
-    parser.add_argument('--retrieval_stride', type=int, default=4) # 4
-    parser.add_argument('--tokenizer', type=str, default=None) # gpt2
-    parser.add_argument('--forbidden_titles', type=str, default=None) # jsons/wikitext_forbidden_titles.txt
+    parser.add_argument(
+        "--data_dir", type=str, default=None
+    )  # '/datastor1/pbansal/huggingface_cache', /home/pb25659/huggingface_cache
+    parser.add_argument("--output_file", type=str, default=None)  #
+    parser.add_argument(
+        "--retrieval_corpus", type=str, default=None
+    )  # wikipedia-dpr-100w
+    parser.add_argument("--query_corpus", type=str, default=None)  # wikipedia-dpr-100w
+    parser.add_argument("--topK", type=int, default=16)  # 16
+    parser.add_argument("--retrieval_query_length", type=int, default=32)  # 32
+    parser.add_argument("--retrieval_stride", type=int, default=4)  # 4
+    parser.add_argument("--tokenizer", type=str, default=None)  # gpt2
+    parser.add_argument(
+        "--forbidden_titles", type=str, default=None
+    )  # jsons/wikitext_forbidden_titles.txt
     args = parser.parse_args()
 
     output_json = get_bm25_documents(args)
 
     Path(args.output_file).parent.mkdir(parents=True, exist_ok=True)
 
-    with open(args.output_file, 'w') as f:
+    with open(args.output_file, "w") as f:
         json.dump(output_json, f)
-
-
--- src/cbralm/zeroshot_rerank.py
+++ src/cbralm/zeroshot_rerank.py
@@ -5,104 +5,146 @@ from pathlib import Path
 from pyserini.search.lucene import LuceneSearcher
 from transformers import AutoModel, AutoTokenizer
 import transformers
-import os 
+import os
 import tqdm
 import argparse
 import json
 
-def MaxSim(query_embed,docs_embed):
+
+def MaxSim(query_embed, docs_embed):
     # query_embed has shape 1 x (# of query tokens) x (rep_dim)
     # docs_embed has shape (# of docs) x (# of query tokens) x (rep_dim)
 
-    query_embed_normalized = query_embed/torch.norm(query_embed,dim=-1).clamp(min=1e-5)[:,:,None]
-    docs_embed_normalized = docs_embed/torch.norm(docs_embed,dim=-1).clamp(min=1e-5)[:,:,None]
+    query_embed_normalized = (
+        query_embed / torch.norm(query_embed, dim=-1).clamp(min=1e-5)[:, :, None]
+    )
+    docs_embed_normalized = (
+        docs_embed / torch.norm(docs_embed, dim=-1).clamp(min=1e-5)[:, :, None]
+    )
 
-    tokenwise_similarity = (query_embed_normalized[:,:,None,:]*docs_embed_normalized[:,None,:,:]).sum(axis=-1) 
-    # shape of tokenwise_similarity is (# of docs) x (# of query tokens) x (# of document tokens). 
+    tokenwise_similarity = (
+        query_embed_normalized[:, :, None, :] * docs_embed_normalized[:, None, :, :]
+    ).sum(axis=-1)
+    # shape of tokenwise_similarity is (# of docs) x (# of query tokens) x (# of document tokens).
     # as an example, if there are 16 retrieved documents for each query, and there are 256 tokens in both query and document,
     # tokenwise_similarity is 16x256x256
 
-    max_over_doctokens_similarity = torch.max(tokenwise_similarity,dim=2).values # max_over_doctokens_similarity has shape (# of docs) x (# of query tokens)
-    sum_over_querytokens_similarity = max_over_doctokens_similarity.sum(dim=1) # sum_over_querytokens_similarity has shape (# of docs)
+    max_over_doctokens_similarity = torch.max(
+        tokenwise_similarity, dim=2
+    ).values  # max_over_doctokens_similarity has shape (# of docs) x (# of query tokens)
+    sum_over_querytokens_similarity = max_over_doctokens_similarity.sum(
+        dim=1
+    )  # sum_over_querytokens_similarity has shape (# of docs)
 
-    nonzero_tokens_query = torch.norm(query_embed_normalized,dim=-1).sum(dim=1)[0] # query_embed_normalized has norm 1 if token is nonzer and 0 if token is zero. 
-    # hence summing over the norms of all query tokens (i.e. over 1) gives the number of non-zero tokens 
-    
-    avg_over_querytokens_similarity = sum_over_querytokens_similarity/nonzero_tokens_query
-    order = torch.argsort(sum_over_querytokens_similarity,descending=True)
+    nonzero_tokens_query = torch.norm(query_embed_normalized, dim=-1).sum(dim=1)[
+        0
+    ]  # query_embed_normalized has norm 1 if token is nonzer and 0 if token is zero.
+    # hence summing over the norms of all query tokens (i.e. over 1) gives the number of non-zero tokens
+
+    avg_over_querytokens_similarity = (
+        sum_over_querytokens_similarity / nonzero_tokens_query
+    )
+    order = torch.argsort(sum_over_querytokens_similarity, descending=True)
 
     return order, avg_over_querytokens_similarity[order].cpu().tolist()
+
 
 def zeroshot_rerank(args):
     reranked_to_retrieved_docs = dict({})
 
-    with open(args.bm25_file,'r') as f : 
+    with open(args.bm25_file, "r") as f:
         doc_retrieval = json.load(f)
-        query_to_retrieved_docs = doc_retrieval['query_to_retrieved_docs']
-        
+        query_to_retrieved_docs = doc_retrieval["query_to_retrieved_docs"]
+
     if args.data_dir:
         datasets.config.DOWNLOADED_DATASETS_PATH = Path(args.data_dir)
         datasets.config.HF_DATASETS_CACHE = Path(args.data_dir)
 
-    match args.rerank_model.split('-')[0]:
-        case 'bert':
-            tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
-        case 'roberta':
-            tokenizer = AutoTokenizer.from_pretrained('roberta-base')
-        case 'gpt2':
-            tokenizer = AutoTokenizer.from_pretrained('gpt2')
-            tokenizer.pad_token = tokenizer.eos_token
-        case default:
-            raise Exception("Use valid rerank_model")
+    if args.rerank_model.split("-")[0] == "bert":
+        tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+    elif args.rerank_model.split("-")[0] == "roberta":
+        tokenizer = AutoTokenizer.from_pretrained("roberta-base")
+    elif args.rerank_model.split("-")[0] == "gpt2":
+        tokenizer = AutoTokenizer.from_pretrained("gpt2")
+        tokenizer.pad_token = tokenizer.eos_token
+    else:
+        raise Exception("Use valid rerank_model")
 
     model = AutoModel.from_pretrained(args.rerank_model).cuda()
     searcher = LuceneSearcher.from_prebuilt_index(args.retrieval_corpus)
 
     for query_info in tqdm.tqdm(query_to_retrieved_docs):
-        start = query_info['begin_location']
-        end = query_info['end_location']
-        query = query_info['query_seg']
-        retrieved_docs = query_info['retrieved_docs']
+        start = query_info["begin_location"]
+        end = query_info["end_location"]
+        query = query_info["query_seg"]
+        retrieved_docs = query_info["retrieved_docs"]
 
         # Need to do some assert to ensure query is the same
 
         assert len(retrieved_docs) > args.topK, "There are not enough retrieved docs"
 
         # Reranking with the ColBert objective
-        sentences = [query] + [json.loads(searcher.doc(doc['docid']).raw())['contents'] for doc in retrieved_docs]
-        tokenized_inputs = tokenizer(sentences,truncation=True,max_length=args.max_length,padding='max_length',return_tensors='pt')
-        model_hidden_states = model(tokenized_inputs['input_ids'].cuda(),attention_mask = tokenized_inputs['attention_mask'].cuda(),output_hidden_states=True)['hidden_states']
-        model_hidden_states = [hidden_state*tokenized_inputs['attention_mask'][:,:,None].cuda() for hidden_state in model_hidden_states]
+        sentences = [query] + [
+            json.loads(searcher.doc(doc["docid"]).raw())["contents"]
+            for doc in retrieved_docs
+        ]
+        tokenized_inputs = tokenizer(
+            sentences,
+            truncation=True,
+            max_length=args.max_length,
+            padding="max_length",
+            return_tensors="pt",
+        )
+        model_hidden_states = model(
+            tokenized_inputs["input_ids"].cuda(),
+            attention_mask=tokenized_inputs["attention_mask"].cuda(),
+            output_hidden_states=True,
+        )["hidden_states"]
+        model_hidden_states = [
+            hidden_state * tokenized_inputs["attention_mask"][:, :, None].cuda()
+            for hidden_state in model_hidden_states
+        ]
 
         reranked_layerwise_docs = dict({})
-        for layer,hidden_state in enumerate(model_hidden_states):
-            order,scores = MaxSim(hidden_state[0:1],hidden_state[1:])
+        for layer, hidden_state in enumerate(model_hidden_states):
+            order, scores = MaxSim(hidden_state[0:1], hidden_state[1:])
             # TODO: Need to add text and title here
-            reranked_layerwise_docs['layer%d'%layer] = [dict({'rank':i,'docid':retrieved_docs[order[i]]['docid'],'score':scores[i]}) for i in range(len(retrieved_docs))]
+            reranked_layerwise_docs["layer%d" % layer] = [
+                dict(
+                    {
+                        "rank": i,
+                        "docid": retrieved_docs[order[i]]["docid"],
+                        "score": scores[i],
+                    }
+                )
+                for i in range(len(retrieved_docs))
+            ]
 
-        query_info['reranked_retrieved_docs'] = reranked_layerwise_docs
+        query_info["reranked_retrieved_docs"] = reranked_layerwise_docs
 
-    doc_retrieval['reranking_args'] = var(args)
+    doc_retrieval["reranking_args"] = var(args)
 
-    return doc_retrieval 
+    return doc_retrieval
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir', type=str, default=None) # '/datastor1/pbansal/huggingface_cache', /home/pb25659/huggingface_cache
-    parser.add_argument('--bm25_file', type=str, default=None) # 
-    parser.add_argument('--retrieval_corpus', type=str, default=None) # wikipedia-dpr-100w
-    parser.add_argument('--reranked_file', type=str, default=None) 
-    parser.add_argument('--rerank_model', type=str, default=None) 
-    parser.add_argument('--topK', type=int, default=16)
-    parser.add_argument('--max_length', type=int,default=256)
+    parser.add_argument(
+        "--data_dir", type=str, default=None
+    )  # '/datastor1/pbansal/huggingface_cache', /home/pb25659/huggingface_cache
+    parser.add_argument("--bm25_file", type=str, default=None)  #
+    parser.add_argument(
+        "--retrieval_corpus", type=str, default=None
+    )  # wikipedia-dpr-100w
+    parser.add_argument("--reranked_file", type=str, default=None)
+    parser.add_argument("--rerank_model", type=str, default=None)
+    parser.add_argument("--topK", type=int, default=16)
+    parser.add_argument("--max_length", type=int, default=256)
     args = parser.parse_args()
 
     output_json = zeroshot_rerank(args)
 
     Path(args.reranked_file).parent.mkdir(parents=True, exist_ok=True)
 
-    with open(args.reranked_file, 'w') as f:
+    with open(args.reranked_file, "w") as f:
         json.dump(output_json, f)
-
-
