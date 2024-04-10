0bdaaeff48008da8da75c060c89b2806fb1730c0
Hprairie <hprairie17@gmail.com>
2024-04-10 17:28:06 -0500

    fixed bug on image

--- visualization-scripts/gpt2_v_bert_summarize.sh
+++ visualization-scripts/gpt2_v_bert_summarize.sh
@@ -1,6 +1,6 @@
 python summarize_results.py \
     --benchmark-dir ./benchmark \
-    --sub-group bm25_retrieval_gpt2_colbert_rerank_layer bm25_retrieval_bert_based_uncased_colbert_rerank_gpt2_layer \
+    --sub-group bm25_retrieval_bert_based_uncased_colbert_rerank_gpt2_layer bm25_retrieval_gpt2_colbert_rerank_layer \
     --sub-group-rename 'Bert-Base' 'GPT-2 S' \
     --layer-lower 0 0\
     --layer-upper 11 11\
